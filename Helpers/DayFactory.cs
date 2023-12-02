5b5e6f7f44468d73e1b3345b4ceb453c7a5bfd4c
Joe Dracup <joe@dracup.me.uk>
2023-12-02 16:56:42 +0000

    removed if statement

--- Helpers/DayFactory.cs
+++ Helpers/DayFactory.cs
@@ -8,13 +8,8 @@ namespace Helpers
         {
             string fullyQualifiedClassName = "Days.Day" + input;
 
-            Type t = Type.GetType(fullyQualifiedClassName);
-
-            if (t == null)
-            {
-                throw new DayDoesNotExistException($"Type {fullyQualifiedClassName} does not exist");
-            }
-
+            Type t = Type.GetType(fullyQualifiedClassName) ?? throw new DayDoesNotExistException($"Type {fullyQualifiedClassName} does not exist");
+            
             try
             {
                 return Activator.CreateInstance(t) as IDay;
