1c6c0778684e7089d4be0c16aba2f5c0d4aa8810
Joe Dracup <joe@dracup.me.uk>
2023-12-03 19:47:12 +0000

    more chat gpt improvements

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -115,29 +115,16 @@ namespace Days
             {
                 return value;
             }
-            else
+
+            var valueMap = new Dictionary<string, int>
             {
-                switch (key)
-                {
-                    case "one":
-                        return 1;
-                    case "two":
-                        return 2;
-                    case "three":
-                        return 3;
-                    case "four":
-                        return 4;
-                    case "five":
-                        return 5;
-                    case "six":
-                        return 6;
-                    case "seven":
-                        return 7;
-                    case "eight":
-                        return 8;
-                    case "nine":
-                        return 9;
-                }
+                {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
+                {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+            };
+
+            if (valueMap.TryGetValue(key, out int mappedValue))
+            {
+                return mappedValue;
             }
 
             throw new ApplicationException(
